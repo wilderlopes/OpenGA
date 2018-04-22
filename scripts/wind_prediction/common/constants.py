7c828418401ce12ff539fb93f47adcafe799ac33
Wilder Lopes <wil@openga.org>
2018-04-22 13:15:45 +0200

    Working on wind prediction.

---
+++ scripts/wind_prediction/common/constants.py
@@ -0,0 +1,15 @@
+# Processing raw data
+# umed: velocidade média zonal (leste-oeste)
+# vmed: velocidade média meridional (norte-sul)
+
+RAWDATAPATH = '../../data/anemometer/rawdata/'
+DATASETPATH = '../../data/anemometer/'
+FILESHOURLY = ['vmed_meridional.txt', 'umed_zonal.txt', 'dirdom.txt', 'rajh.txt']
+FILESDAILY  = ['rajd.txt', 'dirrajd.txt']
+
+# Constants
+DATETIME         = 'datetime'
+V_AVGSPEED       = 'v_avg_speed'
+U_AVGSPEED       = 'u_avg_speed'
+MAINDIRECTION    = 'main_direction'
+MAXGUSTSPEED     = 'max_gust_speed'
---
+++ scripts/wind_prediction/main.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+import common.constants as constants
+from pandas.plotting import scatter_matrix
+
+# Parser
+# parser = argparse.ArgumentParser(
+#     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+#     description="Using GAAFs to predict wind",
+#     epilog="OpenGA - www.openga.org")
+#
+# parser.add_argument('-inputfile', help='Job scheduler accounting file', nargs='+')
+# parser.add_argument('-o', '--output',
+#                     help='Output directory for the report',
+#                     default='report')
+
+
+df = pd.read_csv(constants.DATASETPATH + 'dataset_hourly.csv')
+df[constants.DATETIME] = pd.to_datetime(df[constants.DATETIME])
+df[constants.MAINDIRECTION] =  df.loc[:, constants.MAINDIRECTION].astype(str)
+print(df.dtypes)
+print(df.head())
+
+
+def plot_chart(samples):
+    f, axarr = plt.subplots(2, sharex=True)
+    axarr[0].set_title('Wind speed')
+    axarr[0].scatter(df.index.values[:samples], df[constants.V_AVGSPEED][:samples])
+    axarr[0].set_ylabel(constants.V_AVGSPEED)
+    axarr[1].scatter(df.index.values[:samples], df[constants.V_AVGSPEED][:samples], color='r')
+    axarr[1].set_ylabel(constants.U_AVGSPEED)
+    # axarr.XAxis.set_ticklabels(df.index)
+    plt.show()
+
+# if __name__ == '__main__':
+
+# plot_chart(1000)
+
+# df.hist(column=[constants.V_AVGSPEED, constants.U_AVGSPEED])
+# plt.show()
+
+# Calculate scatter matrix
+scatter_matrix(df[[constants.V_AVGSPEED, constants.U_AVGSPEED]], alpha=0.2, figsize=(6, 6), diagonal='kde')
+plt.show()
