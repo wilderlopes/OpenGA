f8029a009d18b5858959261f6387163905791c9e
Wilder Lopes <wilderlopes@gmail.com>
2018-09-23 13:52:18 +0200

    Organized folder. Improving figure generation in order to have publication-ready pdfs.

--- scripts/wind_prediction/python/main.py
+++ scripts/wind_prediction/python/main.py
@@ -1,49 +1,20 @@
-# Script to call the GAAFs binaries.
-#
-# Author: Wilder Lopes - wil@openga.org
-# www.openga.org
-# Dec 2015
-#
-# The following Python 2.7 script calls the GA-LMS (standard) binary to perform a system identification task.
-#
-# In this example, the multivectors samples belong to the Geometric Algebra of $\mathbb{R}^3$.
-# Thus, each regressor and weight vector entry has 8 coefficients, i.e., for each entry of
-# the weight vector, 8 coefficients have to be estimated. This constrasts with the usal LMS
-# which only estimates real/complex entries. For further information, please refer to the
-# GA documentation at www.openga.org.
-#
-# At the end, the learning curves Mean-Square Error (MSE) and/or Excess Mean-Square Error (EMSE) are plotted.
-
-# The MIT License (MIT)
-#
-# Copyright (c) 2016 onwards, by Wilder Lopes
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 # Start by importing the necessary Python modules:
 import sys, string, os
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from math import log
 from matplotlib.backends.backend_pdf import PdfPages
+import common.constants as constants
+from matplotlib.lines import Line2D
+
+custom_lines = [Line2D([0], [0], color='magenta', linestyle='--', lw=2)]
+
+# Line2D([0], [0], color='blue', lw=2),
+#                 Line2D([0], [0], color='green', lw=2),
+#                 Line2D([0], [0], color='black', lw=2),
 
 # Simulation parameters
 #====================================================
@@ -118,63 +89,65 @@ plt.close()
 
 # Load files EMSE_galms.out and EMSE_theory.out to plot EMSE learning curve and theoretical curve:
 
-f1 = open('EMSE_galms.out', 'r')
-data_label1 = ['EMSE_galms']
-data1_list = []
-for line in f1:
-    data1_list.append(line.rstrip('\n'))
-
-f2 = open('EMSE_theory.out', 'r')
-data_label2 = ['EMSE_theory']
-data2_list = []
-for line in f2:
-    for i in range(len(data1_list)):
-        data2_list.append(line.rstrip('\n'))
-
-data1 = [float(j) for j in data1_list] # Converts to float
-data2 = [float(j) for j in data2_list] # Converts to float
-data1_dB = [10*log(x,10) for x in data1]
-data2_dB = [10*log(x,10) for x in data2]
-
-plt.title('EMSE curves - {}, mu={}, sigma2v={}, sigma2q={}, corr_input={}'.format(BINARY,
-          mu, sigma2v, sigma2q, corr_input), fontsize=9)
-plt.ylabel('EMSE (dB)')
-plt.xlabel('Iterations')
-plt.plot(data1_dB, label = 'EMSE_galms', color = 'r')
-plt.plot(data2_dB, label = 'EMSE_theory', color = 'magenta')
-plt.legend()
-#plt.savefig('EMSE.png', bbox_inches='tight')
-pp.savefig()
-#plt.show()
-
-plt.close()
+# f1 = open('EMSE_galms.out', 'r')
+# data_label1 = ['EMSE_galms']
+# data1_list = []
+# for line in f1:
+#     data1_list.append(line.rstrip('\n'))
+#
+# f2 = open('EMSE_theory.out', 'r')
+# data_label2 = ['EMSE_theory']
+# data2_list = []
+# for line in f2:
+#     for i in range(len(data1_list)):
+#         data2_list.append(line.rstrip('\n'))
+#
+# data1 = [float(j) for j in data1_list] # Converts to float
+# data2 = [float(j) for j in data2_list] # Converts to float
+# data1_dB = [10*log(x,10) for x in data1]
+# data2_dB = [10*log(x,10) for x in data2]
+#
+# plt.title('EMSE curves - {}, mu={}, sigma2v={}, sigma2q={}, corr_input={}'.format(BINARY,
+#           mu, sigma2v, sigma2q, corr_input), fontsize=9)
+# plt.ylabel('EMSE (dB)')
+# plt.xlabel('Iterations')
+# plt.plot(data1_dB, label = 'EMSE_galms', color = 'r')
+# plt.plot(data2_dB, label = 'EMSE_theory', color = 'magenta')
+# plt.legend()
+# #plt.savefig('EMSE.png', bbox_inches='tight')
+# pp.savefig()
+# #plt.show()
+#
+# plt.close()
 
 
 
 # Comparison d versus y
-import pandas as pd
+
 df = pd.read_csv('/home/openga/data/NASA-GRIP/GRIP-MMS/NASA_GRIP_MMS.csv')
 colors = ['orange', 'blue', 'green', 'black']
 blades = [0, 3, 5, 6]
-# cols = ['mock', 'ROLL', 'PIT', 'YAW']
-cols = ['Q', 'U', 'V', 'W']
+# blades = []
+cols = constants.COLS_FOR_SIM
 dic = dict(zip(blades, cols))
 
-plt.figure(figsize=(13.69,8.27))
-plt.title('y curves - {}, mu={}, sigma2v={}, sigma2q={}, corr_input={}'.format(BINARY,
-          mu, sigma2v, sigma2q, corr_input), fontsize=9)
-plt.ylabel('Coefficients')
+# plt.figure(figsize=(13.69,8.27))
+plt.figure(figsize=(6, 3.5))
+# plt.figure()
+# plt.title('y curves - {}, mu={}, sigma2v={}, sigma2q={}, corr_input={}'.format(BINARY,
+#           mu, sigma2v, sigma2q, corr_input), fontsize=9)
+plt.ylabel('mbar')
 plt.xlabel('Iterations')
 for i in range(1):
     f1 = open('y_galms_{}.out'.format(blades[i]), 'r')
-    data_label1 = ['y_galms_{}'.format(blades[i])]
+    # data_label1 = ['y_galms_{}'.format(blades[i])]
     data1_list = []
     for line in f1:
         data1_list.append(line.rstrip('\n'))
     data1 = [float(j) for j in data1_list] # Converts to float
-    plt.scatter(range(len(data1)), df[dic[blades[i]]].values[:len(data1)], label = 'y_galms_{}_{}'.format(blades[i], dic[blades[i]]), color=colors[i], s=3)
+    plt.scatter(range(len(data1)), df[dic[blades[i]]].values[:len(data1)], label = 'actual_{}'.format(dic[blades[i]]), color=colors[i], s=3)
     # plt.plot(df[dic[blades[i]]].values[:len(data1)], label = 'y_galms_{}_{}'.format(blades[i], dic[blades[i]]), color=colors[i], linewidth=3)
-    plt.plot(data1[3:], label = 'y_galms_{}_pred'.format(blades[i]), linestyle='--', color='magenta', linewidth=2)
+    plt.plot(data1[3:], label = 'predicted_{}'.format(dic[blades[i]]), linestyle='--', color='magenta', linewidth=1.5)
 plt.legend(loc='upper left')
 # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
 #            ncol=2, mode="expand", borderaxespad=0.)
@@ -184,25 +157,31 @@ pp.savefig()
 plt.close()
 
 
-plt.figure(figsize=(13.69,8.27))
-plt.title('y curves - {}, mu={}, sigma2v={}, sigma2q={}, corr_input={}'.format(BINARY,
-          mu, sigma2v, sigma2q, corr_input), fontsize=9)
-plt.ylabel('Coefficients')
+plt.figure(figsize=(6, 4.0))
+# plt.figure()
+# plt.title('y curves - {}, mu={}, sigma2v={}, sigma2q={}, corr_input={}'.format(BINARY,
+#           mu, sigma2v, sigma2q, corr_input), fontsize=9)
+plt.ylabel('m/s')
 plt.xlabel('Iterations')
 for i in range(1, len(blades)):
     f1 = open('y_galms_{}.out'.format(blades[i]), 'r')
-    data_label1 = ['y_galms_{}'.format(blades[i])]
+    # data_label1 = ['y_galms_{}'.format(blades[i])]
     data1_list = []
     for line in f1:
         data1_list.append(line.rstrip('\n'))
     data1 = [float(j) for j in data1_list] # Converts to float
-    plt.scatter(range(len(data1)), df[dic[blades[i]]].values[:len(data1)], label = 'y_galms_{}_{}'.format(blades[i], dic[blades[i]]), color=colors[i], s=3)
+    plt.scatter(range(len(data1)), df[dic[blades[i]]].values[:len(data1)], label = 'actual_{}'.format(dic[blades[i]]), color=colors[i], s=3)
     # plt.plot(df[dic[blades[i]]].values[:len(data1)], label = 'y_galms_{}_{}'.format(blades[i], dic[blades[i]]), color=colors[i], linestyle=None, marker='.')
-    plt.plot(data1[3:], label = 'y_galms_{}_pred'.format(blades[i]), linestyle='--', color='magenta', linewidth=2)
-# plt.legend()
+    plt.plot(data1[3:], label = 'predicted_{}'.format(dic[blades[i]]), linestyle='--', color='magenta', linewidth=1.5)
+
+plt.annotate('V', xy=(50, 1400))
+plt.annotate('U', xy=(50, 900))
+plt.annotate('W', xy=(50, 300))
+plt.legend(custom_lines, ['predicted'], loc=2)
+# plt.legend(custom_lines, ['actual_{}'.format(col) for col in cols[1:]] + ['pred'], loc=(0.6, 0.3))
 # plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-           ncol=2, mode="expand", borderaxespad=0.)
+# plt.legend(custom_lines, ['actual_{}'.format(col) for col in cols[1:]], bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+#            ncol=2, mode="expand", borderaxespad=0.)
 #plt.savefig('EMSE.png', bbox_inches='tight')
 pp.savefig()
 plt.close()
